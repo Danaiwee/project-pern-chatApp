a222d9abc80b6865f272b417ff536998077086a7
DanaiWee <Danai_wee@hotmail.com>
2025-02-17 11:03:15 +0700

    Crate backend boilerplate

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.route.js'
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on port 5000")
+});
