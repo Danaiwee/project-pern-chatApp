015e03712dffbbcb48c8a3bfdb6984604a43fe06
DanaiWee <Danai_wee@hotmail.com>
2025-02-18 19:22:43 +0700

    Added socket.io for conversation(sidebar) in client

--- frontend/src/components/Conversation.tsx
+++ frontend/src/components/Conversation.tsx
@@ -1,18 +1,23 @@
+import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
 
 
 const Conversation = ({conversation, emoji}: {conversation: ConversationType, emoji: string}) => {
   const {selectedConversation, setSelectedConversation} = useConversation();
 
-  const online: boolean = true;
+  const {onlineUsers} = useSocketContext();
+
+  const isOnline = onlineUsers.includes(conversation.id);
   const isSelected: boolean = conversation.id === selectedConversation?.id
+  
   return (
     <div 
       className={`w-fit sm:w-full flex items-center hover:bg-sky-300 backdrop-blur-xl bg-opacity-50 transition-all duration-300 rounded-lg p-2 cursor-pointer ${isSelected ? 'bg-sky-400' : ''} gap-2`}
       onClick={() => setSelectedConversation(conversation)}
     >
-      <div className={`avatar ${online ? 'online' : ''} placeholder`}>
-        <div className="w-10 rounded-full">
+      <div className={`relative avatar placeholder`}>
+        {isOnline && <div className='absolute w-2 h-2 bg-green-500 rounded-full' /> }
+        <div className="relative flex w-10 rounded-full">
           <img src={conversation.profilePic} />
         </div>
       </div>
