143adf4f85e0fa81d73aaabf4a52e724d6cc797a
DanaiWee <Danai_wee@hotmail.com>
2025-02-18 18:36:31 +0700

    Added socket.io to server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,11 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js';
+import { app, server } from './socket/socket.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
-const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -16,6 +16,6 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
-    console.log("Server is running on port 5000")
+server.listen(PORT, () => {
+    console.log("Server is running on port: ", PORT);
 });
